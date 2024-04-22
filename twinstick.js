e8ddf5728e41d3c80694b02048dfd72e918063ff
Optional <gitlab.xjt21@aleeas.com>
2024-04-22 11:20:53 +0200

    moving diagonally fixed, shooting possible, nicer colors

--- twinstick.js
+++ twinstick.js
@@ -20,16 +20,17 @@ app.renderer.autoResize = true;
 window.addEventListener('resize', () => { app.renderer.resize(window.innerWidth, window.innerHeight); });
 app.renderer.resize(window.innerWidth, window.innerHeight);
 // Set the background dark grey
-app.renderer.backgroundColor = 0x222222;
+app.renderer.backgroundColor = 0x65635a;
 // Add the canvas that Pixi automatically created for you to the HTML document
 document.body.appendChild(app.view);
 
 // globals
-const lightGrey = 0xaaaaaa;
+const lightGrey = 0xe1ddcf;
 let state = 'load';
 let t;
 let gameContainer;
 let hero;
+const shots = [];
 
 
 // this calculates the new position given the current one
@@ -37,7 +38,7 @@ function calculateMovement(currx, curry, bounds, x, y, speed) {
   let len = Math.sqrt((x * x) + (y * y));
   const point = { x: currx, y: curry };
   // if we're not too fast no need to slow us
-  if (len < speed) {
+  if (len < 1) {
     len = 1;
   }
   if (len !== 0) {
@@ -99,6 +100,83 @@ function calculateRotation(x, y, curr, speed) {
   return goal;
 }
 
+// Moves and rotates hero depending on input
+function handleMovementAndRotation(delta) {
+  // Movement
+  const move = calculateMovement(
+    hero.x, hero.y,
+    hero.height,
+    input.left_x, input.left_y,
+    delta * hero.speed,
+  );
+  hero.x = move.x;
+  hero.y = move.y;
+  // Roatation
+  if (Math.abs(input.right_y) > 0.5 || Math.abs(input.right_x) > 0.5) {
+    // amount we can rotate _this_ frame
+    const speed = hero.rotation_speed * delta;
+    hero.rotation = calculateRotation(input.right_x, -input.right_y, hero.rotation, speed);
+  }
+}
+
+// create shot
+function createShot() {
+  const shot = new Graphics();
+  shot.beginFill(lightGrey);
+  shot.drawRect(0, 0, 6, 12);
+  shot.endFill();
+  shot.pivot.set(3, 25);
+  shot.x = hero.x;
+  shot.y = hero.y;
+  shot.rotation = hero.rotation;
+  shot.dy = -Math.cos(shot.rotation);
+  shot.dx = Math.sin(shot.rotation);
+  shots.push(shot);
+  gameContainer.addChild(shot);
+}
+
+// gets a shot or creates a new one
+function fire() {
+  let needNewShot = true;
+  shots.forEach((shot) => {
+    if (needNewShot && !shot.visible) {
+      const s = shot;
+      s.x = hero.x;
+      s.y = hero.y;
+      s.rotation = hero.rotation;
+      s.dy = -Math.cos(s.rotation);
+      s.dx = Math.sin(s.rotation);
+      s.visible = true;
+      needNewShot = false;
+      return s;
+    }
+    return shot;
+  });
+  if (needNewShot) {
+    createShot();
+  }
+}
+
+// moves visible shots
+function moveShots(delta) {
+  shots.forEach((shot) => {
+    if (shot.visible) {
+      const s = shot;
+      const p = calculateMovement(s.x, s.y, -2 * s.height, s.dx, s.dy, delta * hero.shot_speed);
+      s.x = p.x;
+      s.y = p.y;
+      if (
+        s.x < -s.height || s.x > window.innerWidth + s.height ||
+        s.y < -s.height || s.y > window.innerHeight + s.height
+      ) {
+        s.visible = false;
+      }
+      return s;
+    }
+    return shot;
+  });
+}
+
 // This function is called every frame
 function gameLoop(delta) {
   if (state === 'control') {
@@ -114,14 +192,7 @@ function gameLoop(delta) {
     }
   } else if (state === 'play') {
     getInput();
-    const move = calculateMovement(
-      hero.x, hero.y,
-      hero.height,
-      input.left_x, input.left_y,
-      delta * hero.speed,
-    );
-    hero.x = move.x;
-    hero.y = move.y;
+    handleMovementAndRotation(delta);
     if (input.pause_press) {
       state = 'pause';
       t.text = 'PAUSED';
@@ -130,12 +201,10 @@ function gameLoop(delta) {
       t.visible = true;
       gameContainer.alpha = 0.3;
     }
-    // if we want to turn
-    if (Math.abs(input.right_y) > 0.5 || Math.abs(input.right_x) > 0.5) {
-      // amount we can rotate _this_ frame
-      const speed = hero.rotation_speed * delta;
-      hero.rotation = calculateRotation(input.right_x, -input.right_y, hero.rotation, speed);
+    if (input.fire_down) {
+      fire();
     }
+    moveShots(delta);
   } else if (state === 'pause') {
     getInput();
     if (input.pause_press) {
@@ -194,6 +263,8 @@ loader
 
 // most pixels you can move per frame
 hero.speed = 5;
+// most pixels you can move per frame
+hero.shot_speed = 7;
 // most rads you can turn per frame
 hero.rotation_speed = 0.2;
 
